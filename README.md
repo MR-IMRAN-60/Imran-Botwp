2ba5088ae0657a39b8f1c9871ea3dc114f86a3a4
Imran Ahmed <ahamedemran60@gmail.com>
2024-01-13 03:34:34 +0000

    first Add

---
+++ README.md
@@ -0,0 +1,61 @@
+   # Phoenix-MD 
+<div align="center">
+<a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.demolab.com?font=Ribeye&size=50&pause=1000&color=F710B1&center=true&width=910&height=100&lines=I'M+Phoenix-MD;Multi+Divice+Whatsapp+Bot;Coded+By+Abhishek Suresh" alt="Typing SVG" /></a>
+  
+<p align="center">  
+  <a href="https://youtube.com/channel/UCLUS9v7q4JagAqIJ3eeMM8w">
+    <img alt=Phoenix-MD height="300" src="https://i.ibb.co/tHWJrz3/IMG-20231128-WA0005.jpg">
+   
+</a> 
+    
+</p>
+<p align="center">
+<a 
+
+####  
+Phoenix-MD Multi Device Whatsapp Bot.
+## <sub>[External Plugins](https://github.com/AbhishekSuresh2/External-Plugins)</sub>
+
+***
+
+#### SETUP
+
+Fork The Repo
+    <br>
+<a href="https://github.com/AbhishekSuresh2/Phoenix-MD/fork"><img title="Phoenix-MD" src="https://img.shields.io/badge/FORK Phoenix MD-h?color=black&style=for-the-badge&logo=stackshare"></a>
+
+#### Scan QR Code
+
+<a href="https://phoenix-md-qr.onrender.com/"><img title="Phoenix-MD Scan Qr" src="https://img.shields.io/badge/SCAN QR CODE 1-h?color=black&style=for-the-badge&logo=msi"></a>
+     <br>
+<a href="https://phoenix-md-qr.onrender.com/session"><img title="Phoenix-MD Scan Qr" src="https://img.shields.io/badge/SCAN QR CODE 2-h?color=black&style=for-the-badge&logo=msi"></a>     
+
+
+## Phoenix-MD Deploy Method
+
+
+### Deploy Codespace
+
+<a href="https://github.com/codespaces/new"><img title="Phoenix-MD Deploy CodeSpace" src="https://img.shields.io/badge/DEPLOY CODESPACE-h?color=black&style=for-the-badge&logo=visualstudiocode"></a>
+
+### Deploy Heroku 
+
+<a href="https://heroku.com/deploy?template=https://github.com/AbhishekSuresh2/Phoenix-MD/"><img title="Phoenix-MD Deploy Heroku" src="https://img.shields.io/badge/DEPLOY HEROKU-h?color=black&style=for-the-badge&logo=heroku"></a>
+
+### Deploy Heroku Tutorial
+
+<a href="https://youtu.be/sDojtm-bwN4?si=gbvAqTOSfuVRU2-k"><img title="Heroku Tutorial" src="https://img.shields.io/badge/Heroku Tutorial-h?color=black&style=for-the-badge&logo=heroku"></a>
+### Deploy Railway
+
+<a href="https://railway.app/new"><img title="Phoenix-MD Deploy Railway" src="https://img.shields.io/badge/DEPLOY RAILWAY-h?color=black&style=for-the-badge&logo=Railway"></a>
+
+### Deploy Mogenius
+
+<a href="https://studio.mogenius.com/studio/cloud-space/cloud-space-overview"><img title="Phoenix-MD Deploy Mogenius" src="https://img.shields.io/badge/DEPLOY MOGENIUS-h?color=black&style=for-the-badge&logo=genius"></a> 
+ 
+ ## Support
+
+SUPPORT GROUP: <a href="https://chat.whatsapp.com/BOLb0ICN3sAJ5dloRBw5VD"><img alt="WhatsApp" src="https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white"/></a>
+
+**Star ⭐ The Repo For Amazing Commands**
+
---
+++ plugins/group.js
@@ -0,0 +1,192 @@
+const config = require("../config");
+const { pnix, isPrivate, isAdmin, parsedJid, isUrl } = require("../lib/");
+
+pnix(
+  {
+    pattern: "add ?(.*)",
+    fromMe: isPrivate,
+    desc: "add a person to group",
+    type: "group",
+  },
+  async (message, match) => {
+    if (!message.isGroup)
+      return await message.reply("*_This Command Is Only For Admins_*");
+
+    match = match || message.reply_message.jid;
+    if (!match) return await message.reply("_Mention a User To Add_");
+
+    const isadmin = await isAdmin(message.jid, message.user, message.client);
+
+    if (!isadmin) return await message.reply("*_I M Not An Admin_*");
+    const jid = parsedJid(match);
+
+    await message.client.groupParticipantsUpdate(message.jid, jid, "add");
+
+    return await message.reply(`*_@${jid[0].split("@")[0]} Added_*`, {
+      mentions: [jid],
+    });
+  }
+);
+
+pnix(
+  {
+    pattern: "kick ?(.*)",
+    fromMe: isPrivate,
+    type: "group",
+  },
+  async (message, match) => {
+    if (!message.isGroup)
+      return await message.reply("*_This Command Is Only For Admins_*");
+    if (!isAdmin(message.jid, message.user, message.client))
+      return await message.reply("*_I M Not An Admin_*");
+    match = match || message.reply_message.jid;
+    let jid = parsedJid(match);
+    await message.kick(jid);
+    return await message.reply(`@${jid[0].split("@")[0]} kicked`, {
+      mentions: jid,
+    });
+  }
+);
+
+pnix(
+  {
+    pattern: "promote ?(.*)",
+    fromMe: isPrivate,
+    type: "group",
+  },
+  async (message, match) => {
+    if (!message.isGroup)
+      return await message.reply("*_This Command Is Only For Admins_*");
+    if (!isAdmin(message.jid, message.user, message.client))
+      return await message.reply("*_I M Not An Admin_*");
+    match = match || message.reply_message.jid;
+    let jid = parsedJid(match);
+    await message.promote(jid);
+    return await message.reply(`*_@${jid[0].split("@")[0]} Promoted As An Admin_*`, {
+      mentions: jid,
+    });
+  }
+);
+
+pnix(
+  {
+    pattern: "demote",
+    fromMe: isPrivate,
+    desc: "demote a member",
+    type: "group",
+  },
+  async (message, match) => {
+    if (!message.isGroup)
+      return await message.reply("*_This Command Is Only For Admins_*");
+    match = match || message.reply_message.jid;
+    if (!match) return await message.reply("_Mention A User To Demote");
+    let isadmin = await isAdmin(message.jid, message.user, message.client);
+    if (!isadmin) return await message.reply("*_I M Not An Admin_*");
+    let jid = parsedJid(match);
+    await message.demote(jid);
+    return await message.reply(`*_@${jid[0].split("@")[0]} Demoted From Admin_*`, {
+      mentions: jid,
+    });
+  }
+);
+
+/**
+ * antilink
+*/
+pnix(
+  {
+    on: "text",
+  },
+  async (message, match) => {
+    if (!message.isGroup) return;
+    if (config.ANTILINK)
+      if (isUrl(match)) {
+        await message.reply("_Link Detected_");
+        let botadmin = await isAdmin(message.jid, message.user, message.client)
+        let senderadmin = await isAdmin(message.jid, message.participant, message.client)
+        if (botadmin) {
+          if (!senderadmin){
+          await message.reply(
+            `_Commencing Specified Action :${config.ANTILINK_ACTION}_`
+          );
+          return await message[config.ANTILINK_ACTION]([message.participant]);
+        }} else {
+          return await message.reply("*_I M Not An Admin_*");
+        }
+      }
+  }
+);
+
+pnix(
+  {
+    pattern: "tagall ?(.*)",
+    fromMe: isPrivate,
+    type: "group",
+  },
+  async (message, match) => {
+    if (!message.isGroup) return;
+    const { participants } = await message.client.groupMetadata(message.jid);
+    let teks = "";
+    for (let mem of participants) {
+      teks += ` @${mem.id.split("@")[0]}\n`;
+    }
+    message.sendMessage(teks.trim(), {
+      mentions: participants.map((a) => a.id),
+    });
+  }
+);
+
+pnix(
+  {
+    pattern: "gjid",
+    fromMe: isPrivate,
+    type: "group",
+  },
+  async (message, match, m, client) => {
+    if (!message.isGroup)
+      return await message.reply("*_This Command Is Only For Admins_*");
+    let { participants } = await client.groupMetadata(message.jid);
+    let participant = participants.map((u) => u.id);
+    let str = "╭──〔 *Group Jids* 〕\n";
+    participant.forEach((result) => {
+      str += `├ *${result}*\n`;
+    });
+    str += `╰──────────────`;
+    message.reply(str);
+  }
+);
+
+pnix({
+    pattern: "mute",
+    fromMe: isPrivate,
+    desc: "Mute group",
+    type: "group"
+}, async (message, match, _, client) => {
+    if (!message.isGroup) {
+        return await message.reply("*_This Command Is Only For Admins_*");
+    }
+
+    if (!isAdmin(message.jid, message.user, client)) {
+        return await message.reply("*_This Command Is Only For Admins_*");
+    }
+
+    await message.reply("_Muting_");
+    await client.groupSettingUpdate(message.jid, "announcement", isPrivate);
+});
+
+pnix(
+  {
+    pattern: "unmute",
+    fromMe: isPrivate,
+    desc: "unmute group",
+    type: "group",
+  },
+  async (message, match, m, client) => {
+    if (!message.isGroup)
+      return await message.reply("*_This Command Is Only For Admins_*");
+    if (!isAdmin(message.jid, message.user, message.client))
+      return await message.reply("*_I M Not An Admin_*");
+    await message.reply("_Unmuting_");
+    return await client.groupSettingUpdate(message.jid, "not_announcement");
+  }
+);
